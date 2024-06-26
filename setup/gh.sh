808f38ab94ff29df1aa9812d30b0cd0c81b9ff08
irfan-ahmad-byte <irfan.ahmad.mlka@gmail.com>
2024-06-26 23:57:05 +0500

    chore: Update gh.sh to include set -x for printing commands during execution, correct gh login flag

--- setup/gh.sh
+++ setup/gh.sh
@@ -5,7 +5,7 @@ set -x  # Print commands and their arguments as they are executed
 
 sudo apt update
 
-sudo apt install git
+sudo apt install -y git
 
 (type -p wget >/dev/null || (sudo apt update && sudo apt-get install wget -y)) \
 && sudo mkdir -p -m 755 /etc/apt/keyrings \
@@ -13,15 +13,12 @@ sudo apt install git
 && sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
 && echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
 && sudo apt update \
-&& sudo apt install gh -y
+&& sudo apt install -y gh
 
-#!/bin/bash
-
-# Set Git config based on passed arguments
 if [ -n "$GIT_USERNAME" ] && [ -n "$GIT_EMAIL" ] && [ -n "$GITHUB_TOKEN" ]; then
     git config --global user.name "$GIT_USERNAME"
     git config --global user.email "$GIT_EMAIL"
-    gh auth login --with-token "$GITHUB_TOKEN"
+    echo "$GITHUB_TOKEN" | gh auth login --with-token
     gh auth setup-git
     git config --global credential.helper store
     echo "https://$GIT_USERNAME:$GITHUB_TOKEN@github.com" > ~/.git-credentials
