0f14ad00dbdf06fd6acad09b4f2774ddfbb7a135
irfan-ahmad-byte <irfan.ahmad.mlka@gmail.com>
2024-06-27 12:00:40 +0500

    chore: Update gh.sh to use the latest version of GitHub CLI, deb package

--- setup/gh.sh
+++ setup/gh.sh
@@ -7,13 +7,10 @@ apt update
 
 apt install -y git
 
-(type -p wget >/dev/null || (apt update && apt-get install wget -y)) \
-&& mkdir -p -m 755 /etc/apt/keyrings \
-&& wget -qO- https://cli.github.com/packages/githubcli-archive-keyring.gpg | tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
-&& chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
-&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
-&& apt update \
-&& apt install -y gh
+wget https://github.com/cli/cli/releases/download/v2.52.0/gh_2.52.0_linux_amd64.deb
+dpkg -i gh_2.52.0_linux_amd64.deb
+
+source ~/.bashrc
 
 if [ -n "$GIT_USERNAME" ] && [ -n "$GIT_EMAIL" ] && [ -n "$GITHUB_TOKEN" ]; then
     git config --global user.name "$GIT_USERNAME"
