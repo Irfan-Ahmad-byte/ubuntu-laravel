5f247fbf4815f1cc3a614ef7e12bffeffd9a9a4b
irfan-ahmad-byte <irfan.ahmad.mlka@gmail.com>
2024-06-27 14:17:40 +0500

    Refactor gh.sh to improve security and update GitHub CLI version

--- setup/gh.sh
+++ setup/gh.sh
@@ -12,13 +12,6 @@ dpkg -i gh_2.52.0_linux_amd64.deb
 
 source ~/.bashrc
 
-if [ -z "$1" ]; then
-  echo "Error: GitHub token is not provided."
-  echo "Usage: docker run personalirfan/ubuntu-laravel:latest <github_token>"
-  exit 1
-fi
-
-GITHUB_TOKEN=$1
 
 if [ -n "$GIT_USERNAME" ] && [ -n "$GIT_EMAIL" ]; then
     git config --global user.name "$GIT_USERNAME"
